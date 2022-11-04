6bfcb42e88bece55cdae33587d8798cc6c571ca9
BT_4N2_26 <sifata3@nycstudents.net>
2022-11-04 12:17:23 -0400

    wasd

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
-        StringLoops loops = new StringLoops();
-        System.out.println(loops.countString("an", "Apples and bananas"));
-        System.out.println(loops.countString("tat", "Ratatattat"));
-        System.out.println(loops.countString("lower", "sunflower"));
-        System.out.println(loops.countString("haha", "Hahahahaha"));
-        System.out.println(loops.countString("HAHA", "Hahahahaha"));
-        System.out.println(loops.countString("rain", "it’s the brain drain pain train"));
-        System.out.println(loops.countString("was", "I was about to call you"));
-        System.out.println(loops.countString("but", "I was about to call you"));
-        System.out.println(loops.countString("i", "Supercalifragilisticexpialidocious"));
+import java.util.Scanner;
 
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Choose a secure password that meets these requirements:");
+        System.out.println("- Is at least 8 characters long");
+        System.out.println("- Contains at least one uppercase letter");
+        System.out.println("- Contains at least one lowercase letter");
+        System.out.println("- Contains at least one numeric digit");
+        System.out.println("- Contains at least one of these symbols: ! @ # $ % ^ & * ?");
+        System.out.print("\nEnter your secure password: ");
+        String password = scanner.nextLine();
 
+    /* to be implemented
+       create and use a SecurePassword object with the user’s password and determine
+       if it’s secure (using isSecure()), and if not, provide them a status update
+       (using status()) and prompt them to try again until they have a password that
+       meets all requirements.  Then inform them, "Password is secure" and end.
+
+       Note that, as the user provides updated passwords, you will need to update the
+       password in your SecurePassword object using the setter method, .setPassword(),
+    */
     }
-    }
+}
---
+++ src/SecurePassword.java
@@ -0,0 +1,121 @@
+public class SecurePassword
+{
+    // instance variable
+    private String password;
+
+    // constructor
+    public SecurePassword(String password)
+    {
+        this.password = password;
+    }
+
+    public void setPassword(String newPassword)
+    {
+        password = newPassword;
+    }
+
+    /* Returns true if password:
+       - Is at least 8 characters long
+       - Contains at least one uppercase letter
+       - Contains at least one lowercase letter
+       - Contains at least one numeric digit
+       - Contains at least one of these "special symbols":  ! @ # $ % ^ & * ?
+       Must satisfy ALL FIVE categories for password to be "secure"
+       Return false if any of the above security requirements are not fulfilled.
+    */
+    public boolean isSecure()
+    {
+        if (password.isLongEnough)
+    }
+
+    /* Returns a String that contains information about the security status of the
+       current password.
+
+       If isSecure() is true, this method returns "Password is secure"
+       If isSecure() is false, this methods should return a single String that informs the
+       user of which security requirements are not currently being met.
+
+       For example, the password 3WrT6tH does not meet length or special symbol
+       requirements, so this method should return the following String (using a line
+       break \n):
+
+       "The password must be at least 8 characters
+        The password must contain a special symbol: ! @ # $ % ^ & * ?"
+    */
+    public String status()
+    {
+
+    }
+
+
+    // PRIVATE HELPER METHODS (marked "private" rather than "public")
+
+    /* Returns true if the password’s length meets the minimum requirement of 8 characters
+       and false otherwise.
+     */
+    private boolean isLongEnough()
+    {
+        if (password.length() >= 8) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Returns true if the password has at least one uppercase letter and false otherwise.
+     */
+    private boolean containsUppercase()
+    {
+        /* this one is completed for you as a hint for how to do the others! */
+        String upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        return checkString(upperCaseLetters);
+    }
+
+    /* Returns true if the password has at least one lowercase letter and false otherwise.
+     */
+    private boolean containsLowercase()
+    {
+        String upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lowerCaseLetters = upperCaseLetters.toLowerCase();
+        return checkString(lowerCaseLetters);
+    }
+
+    /* Returns true if the password has at least one digit and false otherwise.
+     */
+    private boolean containsDigit()
+    {
+        String numbers = "0123456789";
+        return checkString(numbers);
+    }
+
+    /* Returns true if the password has at least one of these special symbols:
+       ! @ # $ % ^ & * ?    and false otherwise.
+     */
+    private boolean containsSpecialSymbol()
+    {
+        String specialChar = "!@#$%^&*";
+        return checkString(specialChar);
+    }
+
+    /* Checks a given character string to see if password contains at least one
+       character from that string.
+
+       For example, if characterString is "ABCDEFGH", and password == "jbHFmfA"
+       this method will find two occurrences (F and A) and return true, since two
+       is at least one
+     */
+    private boolean checkString(String characterString)
+    {
+        int count = 0;
+        for (int i = 0; i != password.length(); i++) {
+            if (password.indexOf(password.charAt(i)) != -1  ) {
+                count++;
+            }
+            i++;
+
+        }
+        if (count >= 1){
+            return true;
+        }
+        return false;
+    }
+}
